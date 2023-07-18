bdf5397a4db2e3c34ede6e0998d4b329f48ea798
justallyt <albertjunk2030@gmail.com>
2023-07-18 23:02:57 +0300

    Updates

--- client/src/components/Admin/AnalyticsSection.jsx
+++ client/src/components/Admin/AnalyticsSection.jsx
@@ -1,4 +1,39 @@
 import { GiTakeMyMoney } from "react-icons/gi"
+import{HiArrowSmallDown, HiArrowSmallUp} from "react-icons/hi2"
+import { BarChart, Bar,  XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+//Dummy data for bar chart
+const data = [
+    {
+        name: 'March 1 - 31',
+        earnings: 25000,
+        assets: 11165,
+        amt: 40,
+    },
+    {
+     name: 'April 1 - 30',
+     earnings: 31500,
+     assets: 14080,
+     amt: 40,
+   },
+   {
+     name: 'May 1 - 31',
+     earnings: 67300,
+     assets: 31800,
+     amt: 29,
+   },
+   {
+      name: 'June 1 - 30',
+      earnings: 98300,
+      assets: 54200,
+      amt: 50
+   }
+]
+
+const pie_data = [
+    { name: 'New Brands', value: 100},
+    { name: 'Brand Purchases', value: 450}
+]
+const COLORS = ['#0088FE', '#00C49F',]
 const AnalyticsSection = () => {
   return (
     <div className="analytics-section">
@@ -14,11 +49,24 @@ const AnalyticsSection = () => {
                                                                  <option value="March 2023">March 2023</option>
                                                                  <option value="April 2023">April 2023</option>
                                                                  <option value="May 2023">May 2023</option>
+                                                                 <option value="June 2023">June 2023</option>
                                                         </select>
                                                  </form>
                                     </div>
                                    <div className="updates-map-texts">
-                                              <div className="update-map-container"></div>
+                                              <div className="update-map-container">
+                                                       <ResponsiveContainer width="100%" height="100%">
+                                                               <BarChart data={data} barGap={10} >
+                                                                        <CartesianGrid vertical={false} strokeDasharray="2 2" opacity={0.4} />
+                                                                                    <XAxis dataKey="name" tickLine={false} axisLine={false} label={{ fontSize:15}}  tick={{ fontSize: "12px"}} />
+                                                                                    <YAxis axisLine={false} tickLine={false} tick={{ fontSize: "12px"}} />
+                                                                                    <Tooltip shared={false} /> 
+                                                                                   <Legend />
+                                                                                    <Bar dataKey="assets" fill="#FFC122" opacity={0.8}  radius={[10, 10, 5, 5]} />
+                                                                                    <Bar dataKey="earnings" fill="#00cde0" radius={[10, 10, 5, 5]} />
+                                                                 </BarChart>
+                                                       </ResponsiveContainer>
+                                              </div>
                                             <div className="time-descriptions">
                                                         <div className="money-part">
                                                                     <span><GiTakeMyMoney /></span>
@@ -36,7 +84,7 @@ const AnalyticsSection = () => {
                                                                                 </div>
                                                                      </div>
                                                                      <div className="assets-count">
-                                                                                <span></span>
+                                                                                <span className="asset"></span>
                                                                                 <div className="asset-number">
                                                                                            <h4>Assets Bought this month</h4>
                                                                                            <h2>356</h2>
@@ -53,7 +101,33 @@ const AnalyticsSection = () => {
                        </div>
               </div>
               <div className="date-analytics">
+                          <div className="brand-box">
+                                   <div className="brand-box-content">
+                                               <h3>25 New Brands</h3>
+                                               <div className="percent">
+                                                           <span><HiArrowSmallUp /></span>
+                                                           <p className="profit">+9% from last month</p>
+                                               </div>
 
+                                               <h3>Brand Purchases</h3>
+                                                <p>343 Video Assets</p>
+                                                <div className="percent">
+                                                            <span><HiArrowSmallDown /></span>
+                                                            <p className="loss">-18% from last month</p>
+                                                </div>
+                                   </div>
+                                   <div className="pie-chart">
+                                              <ResponsiveContainer width="100%" height="100%">
+                                                      <PieChart width={200} height={200}>
+                                                             <Pie data={pie_data} cx={60} cy={90} innerRadius={40} outerRadius={60} fill="#8884d8" dataKey="value">
+                                                                       { pie_data.map((entry, index) => (
+                                                                               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                                                       ))}
+                                                             </Pie>
+                                                      </PieChart>
+                                              </ResponsiveContainer>
+                                   </div>
+                          </div>
               </div>
     </div>
   )
