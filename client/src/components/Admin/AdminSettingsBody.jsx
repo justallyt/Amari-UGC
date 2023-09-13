f8172ff6e9a33dd5923f11f6fc7dc71e97751b5b
justallyt <albertjunk2030@gmail.com>
2023-09-13 19:36:23 +0300

    Updates

--- client/src/components/Admin/AdminSettingsBody.jsx
+++ client/src/components/Admin/AdminSettingsBody.jsx
@@ -1,3 +1,7 @@
+import { HiOutlineUserCircle } from "react-icons/hi2"
+import { IoMdNotificationsOutline } from "react-icons/io"
+import { FaWpforms } from "react-icons/fa6"
+import { GrShieldSecurity } from 'react-icons/gr'
 
 const AdminSettingsBody = () => {
   return (
@@ -9,10 +13,27 @@ const AdminSettingsBody = () => {
 
                          <div className="admin-settings-types">
                                      <div className="types-header">
-                                                <div className="type">
-                                                           <span></span>
+                                                <div className="type active">
+                                                           <span><HiOutlineUserCircle /></span>
                                                            <p>Profile</p>
                                                 </div>
+                                                <div className="type">
+                                                           <span><IoMdNotificationsOutline /></span>
+                                                           <p>Notifications</p>
+                                                </div>
+                                                <div className="type">
+                                                           <span><FaWpforms /></span>
+                                                           <p>Bills</p>
+                                                </div>
+                                                <div className="type">
+                                                           <span><GrShieldSecurity /></span>
+                                                           <p>Security</p>
+                                                </div>
+                                     </div>
+
+
+                                     <div className="settings-tabs-container">
+                                              
                                      </div>
                          </div>
               </div>
